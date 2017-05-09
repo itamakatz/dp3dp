4b6f570bc89e9f6be783808baf14467cc185d76b
liavst2 <liavst2@gmail.com>
2017-05-09 19:28:47 +0300

    updated intermediator.ino

--- Firmware/intermediator/intermediator.ino
+++ Firmware/intermediator/intermediator.ino
@@ -1,14 +1,33 @@
 
 #define ENABLED 0     // for stepper driver A4988 - enable with low signal
-#define DISABLES 1
+#define DISABLED 1
 
-const int RECIEVE_ENABLE = A0; // connected to Z_ENABLE_PIN 62 of the mega
+const int RECIEVE_ENABLE = 7; // connected to Z_ENABLE_PIN 62 of the mega
 const int RECIEVE_STEP = 2;    // connected to Z_STEP_PIN 46 of the mega
 const int RECIEVE_DIR = 4;     // connected to Z_DIR_PIN 48 of the mega
 
-const int PASS_ENABLE = A1;  // connected to ENABLE pin 2 of the stepper driver 
-const int PASS_STEP = 13;    // connected to STEP pin 16 of the stepper driver 
-const int PASS_DIR = 12;     // connected to DIR pin 19 of the stepper driver 
+const int PASS_ENABLE = 8;    // connected to ENABLE pin 2 of the stepper driver 
+const int PASS_STEP = 13;      // connected to STEP pin 16 of the stepper driver 
+const int PASS_DIR = 12;       // connected to DIR pin 19 of the stepper driver 
+
+
+volatile byte state = DISABLED;
+
+
+
+void toggle_enabled_pin(){
+	state = !state;
+	digitalWrite(PASS_ENABLE, state);
+}
+
+
+void step_received(){
+	if (state == ENABLED){
+		digitalWrite(PASS_DIR, digitalRead(RECIEVE_DIR));
+		digitalWrite(PASS_STEP, digitalRead(RECIEVE_STEP));
+	}
+}
+
 
 
 void setup(){
@@ -22,17 +41,16 @@ void setup(){
 	pinMode(PASS_ENABLE, OUTPUT);
 	pinMode(PASS_STEP, OUTPUT);
 	pinMode(PASS_DIR, OUTPUT);
+
+	attachInterrupt(digitalPinToInterrupt(RECIEVE_ENABLE), toggle_enabled_pin, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(RECIEVE_STEP), step_received, CHANGE);
+
 }
 
 
 void loop(){
 
-	int state = digitalRead(RECIEVE_ENABLE);
-	if (state == ENABLED){
-		digitalWrite(PASS_ENABLE, ENABLED);
-		digitalWrite(PASS_STEP, digitalRead(RECIEVE_STEP));
-		digitalWrite(PASS_DIR, digitalRead(RECIEVE_DIR));
-	} else {
+	if (state == DISABLED){
 		// do our stuff
 	}
 
