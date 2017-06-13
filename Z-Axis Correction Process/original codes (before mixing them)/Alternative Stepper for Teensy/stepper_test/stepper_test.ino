698d642318d5398c0d51cb41f696045573d345fa
Itamar Katz <itamakatz@gmail.com>
2017-06-13 20:28:59 +0300

    played with the steper_test and found the constraints of a steper motor

--- Z-Axis Correction Process/original codes (before mixing them)/Alternative Stepper for Teensy/stepper_test/stepper_test.ino
+++ Z-Axis Correction Process/original codes (before mixing them)/Alternative Stepper for Teensy/stepper_test/stepper_test.ino
@@ -4,25 +4,59 @@
 #define ENABLE_PIN 2
 #define STEP_PIN   3
 #define DIR_PIN    4
+#define E0		   5
+#define E1		   6
+#define E2		   7
+
+// 60[s/min] * 1000000[us/s] / microsteps / steps / rpm
+// 60[s/min] * 1000000[us/s] / microsteps [1/step] / steps [step] / rpm [200 * step / min]
+// => [us/min]
+// => [(us * step)/min]
+// => [us/min]
+// => [us/step]
 
 
-DRV8825 stepper(2000, DIR_PIN, STEP_PIN, ENABLE_PIN);
+// DRV8825 stepper(2000, DIR_PIN, STEP_PIN, ENABLE_PIN);
+DRV8825 stepper1(125, DIR_PIN, STEP_PIN, ENABLE_PIN, E0, E1, E2);
+DRV8825 stepper2(80, DIR_PIN, STEP_PIN, ENABLE_PIN, E0, E1, E2);
 
 
 void setup(){
 	Serial.begin(9600);
-	stepper.setMicrostep(1);
+	stepper1.setMicrostep(16);
+	stepper1.setRPM(80);
 }
-   
-
+ 
 void loop(){
-    stepper.rotate(233);
+    stepper1.rotate(233);
     delay(1000);
-    stepper.rotate(-317);
+    stepper1.rotate(-317);
     delay(1000);
-    stepper.move(360);
+    stepper1.move(360);
     delay(1000);
-    
+}  
 
+void setup2(){
+	Serial.begin(9600);
 }
 
+void loop2(){
+
+	stepper1.setMicrostep(16);
+    
+    stepper1.rotate(233);
+    delay(1000);
+    stepper1.rotate(-317);
+    delay(1000);
+    stepper1.move(360);
+    delay(1000);
+
+	stepper2.setMicrostep(32);
+
+    stepper2.rotate(233);
+    delay(1000);
+    stepper2.rotate(-317);
+    delay(1000);
+    stepper2.move(360);
+    delay(1000);
+}
