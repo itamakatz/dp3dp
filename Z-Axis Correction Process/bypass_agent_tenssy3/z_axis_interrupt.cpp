311f63e1c5448d90497cdb499fe2ef93a9a40020
Itamar Katz <itamakatz@gmail.com>
2017-06-20 14:09:32 +0300

    viao 2

--- Z-Axis Correction Process/bypass_agent_tenssy3/z_axis_interrupt.cpp
+++ Z-Axis Correction Process/bypass_agent_tenssy3/z_axis_interrupt.cpp
@@ -15,31 +15,37 @@ void intermediator_setup_z(){
 	pinMode(PASS_STEP_Z, OUTPUT);
 	pinMode(PASS_DIR_Z, OUTPUT);
 
-	attachInterrupt(digitalPinToInterrupt(RECIEVE_ENABLE_Z), enable_received_z, CHANGE);
+	// attachInterrupt(digitalPinToInterrupt(RECIEVE_ENABLE_Z), enable_received_z, CHANGE);
 	attachInterrupt(digitalPinToInterrupt(RECIEVE_DIR_Z), dir_received_z, CHANGE);
 	attachInterrupt(digitalPinToInterrupt(RECIEVE_STEP_Z), step_received_z, CHANGE);
 
 	digitalWriteFast(PASS_ENABLE_Z, digitalReadFast(RECIEVE_ENABLE_Z));
 	digitalWriteFast(PASS_DIR_Z, digitalReadFast(RECIEVE_DIR_Z));
 
-	enable_state = digitalReadFast(RECIEVE_ENABLE_Z);
+	// enable_state = digitalReadFast(RECIEVE_ENABLE_Z);
+	enable_state = DISABLED;
+	digitalWriteFast(PASS_ENABLE_Z, enable_state);
 
 	sei();
 
 	// delay(INTERMEDIATOR_DELAY);
 }
 
-void enable_received_z() {
-	enable_state = digitalReadFast(RECIEVE_ENABLE_Z);
-	digitalWriteFast(PASS_ENABLE_Z, enable_state);
+// void enable_received_z() {
+// 	enable_state = digitalReadFast(RECIEVE_ENABLE_Z);
+// 	digitalWriteFast(PASS_ENABLE_Z, enable_state);
 
-	#ifdef DEBUG_INTERRUPTS_Z
-		Serial.println(F("enable_received_z"));
-	#endif
-}
+// 	#ifdef DEBUG_INTERRUPTS_Z
+// 		Serial.println(F("enable_received_z"));
+// 	#endif
+// }
 
 void step_received_z() {
+	digitalWriteFast(PASS_ENABLE_Z, ENABLED);
+	// enable_state = ENABLED;
 	digitalWriteFast(PASS_STEP_Z, digitalReadFast(RECIEVE_STEP_Z));
+	digitalWriteFast(PASS_ENABLE_Z, DISABLED);
+	// enable_state = DISABLED;
 
 	#ifdef DEBUG_INTERRUPTS_Z
 		Serial.println(F("step_received_z"));
@@ -47,7 +53,11 @@ void step_received_z() {
 }
 
 void dir_received_z() {
+	digitalWriteFast(PASS_ENABLE_Z, ENABLED);
+	// enable_state = ENABLED;
 	digitalWriteFast(PASS_DIR_Z, digitalReadFast(RECIEVE_DIR_Z));
+	// enable_state = DISABLED;
+	digitalWriteFast(PASS_ENABLE_Z, DISABLED);
 
 	#ifdef DEBUG_INTERRUPTS_Z
 		Serial.println(F("dir_received_z"));
